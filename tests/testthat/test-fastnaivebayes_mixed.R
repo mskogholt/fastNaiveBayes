b2b5a0ed4d4aa804487ced72d5e559ac3dba9fb2
Martin Skogholt <m.skogholt@gmail.com>
2019-03-14 22:42:22 +0100

    [BOYSCOUT] Now finally 100% test coverage

--- tests/testthat/test-fastnaivebayes_mixed.R
+++ tests/testthat/test-fastnaivebayes_mixed.R
@@ -54,20 +54,24 @@ test_that("Mixed event models estimation gives expected results when mixed", {
 
   y <- as.factor(c("Ham", "Ham", "Ham", "Spam", "Spam", "Spam"))
 
-  x1 <- matrix(c(2, 3, 2, 2, 3, 2), nrow = 6, ncol = 1)
-  colnames(x1) <- c("wo")
+  x1 <- matrix(c(2, 3, 2, 2, 3, 2,
+                 2, 3, 2, 2, 3, 2), nrow = 6, ncol = 2)
+  colnames(x1) <- c("wo","lo")
 
-  x2 <- matrix(c(1, 0, 1, 1, 0, 1), nrow = 6, ncol = 1)
-  colnames(x2) <- c("no")
+  x2 <- matrix(c(1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1), nrow = 6, ncol = 2)
+  colnames(x2) <- c("no","mo")
 
-  x3 <- matrix(c(2.8, 2.7, 2.9, 2.9, 2.7, 2.8), nrow = 6, ncol = 1)
-  colnames(x3) <- c("go")
+  x3 <- matrix(c(2.8, 2.7, 2.9, 2.9, 2.7, 2.8,
+                 2.8, 2.7, 2.9, 2.9, 2.7, 2.8), nrow = 6, ncol = 2)
+  colnames(x3) <- c("go","ho")
 
   x <- cbind(x1, x2, x3)
-  col_names <- c("wo", "no", "go")
-  colnames(x) <- col_names
 
   mod <- fastNaiveBayes.mixed(x, y, laplace = 0.00001, sparse = TRUE)
-  expect_warning(predict(mod, newdata=x, type = "class"))
 
+  new_preds <- predict(mod, newdata=x[,c(1,3,5)], type = "raw")
+  other_preds <- predict(mod, newdata = x[,c(1,3,5)], type = "raw", sparse = TRUE)
+
+  expect_equal(sum(abs(new_preds-other_preds)), 0)
+  expect_warning(predict(mod, newdata=x, type = "class"))
 })
